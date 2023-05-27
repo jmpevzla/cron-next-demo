41c45983630d32e0d17e6a86beb4aa60e3a89798
Jose Perez <jmperezvzla@gmail.com>
2023-05-27 14:09:39 -0400

    Fix require to import

--- src/pages/api/_cron_file.js
+++ src/pages/api/_cron_file.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+import { appendFile } from 'fs';
 
 export async function updateFile() {
     const date = new Date();
     return new Promise((res, rej) => {
-        fs.appendFile('/tmp/FILE.txt', date.toISOString() + '\n', function(err) {
+        appendFile('/tmp/FILE.txt', date.toISOString() + '\n', function(err) {
             if (err) {
                 return rej(err);
             }
