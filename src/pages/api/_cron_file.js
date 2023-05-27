e365074f50f6d911138e020247e0671d7de2dd06
Jose Perez <jmperezvzla@gmail.com>
2023-05-27 14:07:59 -0400

    move code of file to _cron_file

--- src/pages/api/_cron_file.js
+++ src/pages/api/_cron_file.js
@@ -1,4 +1,16 @@
-import { updateFile } from '../jobs/file';
+const fs = require('fs');
+
+export async function updateFile() {
+    const date = new Date();
+    return new Promise((res, rej) => {
+        fs.appendFile('/tmp/FILE.txt', date.toISOString() + '\n', function(err) {
+            if (err) {
+                return rej(err);
+            }
+            res();
+        });
+    });
+}
 
 export default async function handler(req, res) {
     await updateFile();
